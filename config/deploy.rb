4273f6384da03e887210ee231117f104a5ed5620
mihon <mihon@zencoders.ru>
2015-07-02 16:36:59 +0300

    Remove useless code from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,24 +11,3 @@ set :repo_url, 'https://github.com/diversiya/mihon.semehin.ru.git'
 set :deploy_to, '/home/mihon/mihon.semehin.ru'
 
 set :keep_releases, 3
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  task :start_server do
-  	on roles(:app, :web) do
-  		within release_path do
-		  	
-		  end
-		end
-  end
-
-end
